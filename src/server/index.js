04ed2f3d8c7220ad7d104f95353c9c22adfcd6ea
Jonathan Root <jonathanroot15@gmail.com>
2020-09-08 03:03:30 -0700

    add cool faces

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,28 @@
-var path = require('path')
-const express = require('express')
+var path = require('path');
+const cool = require('cool-ascii-faces');
+const express = require('express');
 const cors = require('cors');
-const mockAPIResponse = require('./mockAPI.js')
 const dotenv = require('dotenv');
 var aylien = require("aylien_textapi");
+const PORT = process.env.PORT || 5000;
 dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(express.static('dist'))
+// Heroku configuration to push locally
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+app.use(express.static('dist'));
 
 /* Middleware*/
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -31,20 +42,16 @@ var textapi = new aylien({
     application_key: process.env.API_KEY
   });  
 
-
 app.get('/aylien', function(req, res) {
   textapi.sentiment(data, function(error, response) {
     if (error === null) {
       STORE.push(response)
       res.send(STORE);
-      console.log(STORE)
     }
   });  
 })
 
-
 app.post('/url', function(req,res) {
-  // URL.push(req.body);
   data = req.body;
   res.json({
     status: "success",
@@ -52,19 +59,12 @@ app.post('/url', function(req,res) {
   })
 });  
 
-// app.get('/all', function (req, res) {
-//   // console.log(STORE)
-//     res.send(STORE)
-// })
-
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
-})
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
-})
-
-
+});
